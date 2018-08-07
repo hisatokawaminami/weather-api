5b3749aab287c7b079fd90a1c0d92658b120a8f4
Austin Barr and Hisato Kawaminami <austin@>
2018-08-07 12:48:18 -0700

    add weather

---
+++ src/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+        $('.description').text(`${response.weather[0].description}`);
+      },
+      error: function() {
+        $('.errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+entry: './src/main.js',
+output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, 'dist')
+},
+devtool: 'bundle.js',
+devServer: {
+  contentBase: './dist'
+},
+plugins: [
+  new Dotenv(),
+  new CleanWebpackPlugin(['dist']),
+  new HtmlWebpackPlugin({
+    title: 'Triangle Finder',
+    template: './src/index.html',
+    inject: 'body'
+  })
+],
+module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude:[
+        /node_modules/,
+        /spec/
+      ],
+      loader: "eslint-loader"
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    },
+    {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ['es2015']
+        }
+      }
+  ]
+}
+};
